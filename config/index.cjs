93744a74def4411b6d1971fe69b420f2bd69d9f4
BDJSABJD <157972997+BDJSABJD@users.noreply.github.com>
2024-01-28 23:32:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -16,16 +16,16 @@ const USER_CONFIG = {
   PROVINCE: '广东',
   CITY: '广州',
 
-  USERS: [
+  USERS: [派大星
     {
       // 想要发送的人的名字
-      name: '医保',
+      name: '华仪',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o-OIL6updXL_IP03e0U4H8b3ojvg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'I_NCNrqL3ab5EGsl5Uq7McWZTWHJlVCOmJN7XiE6On8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '11-26',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'I_NCNrqL3ab5EGsl5Uq7McWZTWHJlVCOmJN7XiE6On8',
+  CALLBACK_TEMPLATE_ID: 'o-OIL6updXL_IP03e0U4H8b3ojvg',
 
   CALLBACK_USERS: [派大星
     {
-      name: '自己',
+      name: '派大星',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o-OIL6updXL_IP03e0U4H8b3ojvg',
     }
   ],
 
